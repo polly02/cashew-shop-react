8cdb0f5ff5951a1c25886ca2944d7f2669cf378c
polina <polina.zdanovich.02@mail.ru>
2023-01-31 22:52:23 +0300

    shop-react category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,11 +1,32 @@
 import style from "./Category.module.css"
 
-function Category(){
-    return(
+function Category() {
+    return (
         <>
             <h1>Categories</h1>
 
-            <div></div>
+            <div className={style["wrapper"]}>
+                <div className={style["macbook"]}>
+                    <div className={style["img"]}></div>
+                    <div className={style["btn"]}>Electronics</div>
+                </div>
+                <div className={style["boot"]}>
+                    <div className={style["img"]}></div>
+                    <div className={style["btn"]}>Fashion & accessories</div>
+                </div>
+                <div className={style["perfume"]}>
+                    <div className={style["img"]}></div>
+                    <div className={style["btn"]}>Beauty & health</div>
+                </div>
+                <div className={style["plant"]}>
+                    <div className={style["img"]}></div>
+                    <div className={style["btn"]}>Home & furniture</div>
+                </div>
+
+                <div className={style["btn-view-all"]}>View all
+                    <div className={style["arrow"]}></div>
+                </div>
+            </div>
         </>
     )
 }
